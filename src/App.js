823448b7d77ed760dc681f573b5c55a37769443b
SiddhiApraj7 <2021mcb1247@iitrpr.ac.in>
2023-07-27 19:13:40 +0530

    button color change

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App() {
         <div className="my-auto p-6">
           <h1 className="text-6xl font-bold text-[#dde3fe] ml-8">GDSC Management Portal</h1>
           <div className="mt-6 ml-8 flex gap-2">
-            <button className="bg-[#dde3fe] rounded-3xl font-medium px-4 py-2 border-[#3243c0] border-2">Join a Project</button>
-            <button className="bg-[#dde3fe] rounded-3xl font-medium px-4 py-2 border-[#3243c0] border-2">Host a project</button>
+            <button className="bg-[#dde3fe] rounded-3xl font-medium px-4 py-2 border-[#3142c0] text-[#3142c0] border-2">Join a Project</button>
+            <button className="bg-[#dde3fe] rounded-3xl font-medium px-4 py-2 border-[#3142c0] text-[#3142c0] border-2">Host a project</button>
           </div>
         </div>
       <img src="illus.jpg" className="h-1/3 w-2/3"></img>
