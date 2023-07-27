50bfd18429fe1c7d0f5bbf0c47ad61ace51488bb
Anushtha <anushthaprakash@gmail.com>
2023-07-27 19:36:08 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+
+
+function App() {
+  return (
+    <div className="">
+<div className='bg-gray-100 h-screen'>
+  <div className="flex justify-between bg-white">
+    <div className="flex p-2">
+    <img src="gdsc-logo.png" className="h-20 w-20 rounded-full" alt="logo of gdsc"></img>
+      <h1 className="text-4xl py-6 font-bold italic p-4 text-blue-900">GDSC Management Portal</h1>
+    
+    </div>
+    <div className="flex gap-6 py-6 px-4 font-bold text-blue-900">
+    <h1 className="my-auto text-xl">About Us</h1>
+    <h1 className="my-auto text-xl">Contact</h1>
+    <h1 className="my-auto text-xl">Login</h1>
+    </div>
+ 
+  </div>
+ </div>
+    </div>
+    
+  );
+}
+
+export default App;
