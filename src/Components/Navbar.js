d98b76d8fdc35921ade6cc51b626b71927cc0889
SiddhiApraj7 <2021mcb1247@iitrpr.ac.in>
2023-07-27 23:58:34 +0530

    projects page

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className="pt-8 mx-auto text-lg font-bold text-[#dde3fe] flex gap-20 ">
+    <div className="pt-8 mx-auto text-lg font-bold text-[#dde3fe] flex gap-20 mb-3">
           <h1 className="text-center">Home</h1>
           <h1>About</h1>
           <h1>Contact</h1>
