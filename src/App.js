7f84de1f6ccd8bf9f401783f5c89e6b2f9aa5247
tanisha <tanisha22daharwal@gmail.com>
2023-07-27 19:05:30 +0530

    landing page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="bg-[#7a8aff] h-full flex flex-col justify-center items-center" >
      
-        <div className="mt-8 text-lg font-bold text-[#dde3fe] flex gap-20 ">
+        <div className="pt-8 mx-auto text-lg font-bold text-[#dde3fe] flex gap-20 ">
           <h1 className="text-center">Home</h1>
           <h1>About</h1>
           <h1>Contact</h1>
@@ -14,8 +14,8 @@ function App() {
       
       <div className="flex justify-between">
         <div className="my-auto p-6">
-          <h1 className="text-6xl font-bold text-[#dde3fe] ml-10">GDSC Management Portal</h1>
-          <div className="mt-6 ml-10 flex gap-2">
+          <h1 className="text-6xl font-bold text-[#dde3fe] ml-8">GDSC Management Portal</h1>
+          <div className="mt-6 ml-8 flex gap-2">
             <button className="bg-[#dde3fe] rounded-3xl font-medium px-4 py-2 border-[#3243c0] border-2">Join a Project</button>
             <button className="bg-[#dde3fe] rounded-3xl font-medium px-4 py-2 border-[#3243c0] border-2">Host a project</button>
           </div>
