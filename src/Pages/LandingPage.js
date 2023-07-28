389c7e5d23e4940c7ba871923ddcc000254f3554
Anushtha <anushthaprakash@gmail.com>
2023-07-29 00:48:48 +0530

    partial landing page done

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,24 +1,17 @@
-import React from 'react'
-import Navbar from '../Components/Navbar'
+import React from "react";
+import Hero from "../Components/Hero";
+import Footer from "../Components/Footer";
 
 export default function LandingPage() {
   return (
-    <div className="bg-[#7a8aff] h-full flex flex-col justify-center items-center" >
-     <Navbar />
-        
-      
-      <div className="flex justify-between">
-        <div className="my-auto p-6">
-          <h1 className="text-6xl font-bold text-[#dde3fe] ml-8">GDSC Management Portal</h1>
-          <div className="mt-6 ml-8 flex gap-2">
-            <button className="bg-[#dde3fe] rounded-3xl font-medium px-4 py-2 border-[#3142c0] text-[#3142c0] border-2">Join a Project</button>
-            <button className="bg-[#dde3fe] rounded-3xl font-medium px-4 py-2 border-[#3142c0] text-[#3142c0] border-2">Host a project</button>
-          </div>
-        </div>
-      <img src="new_illus.png" className="h-1/3 w-[63%]"></img>
-      </div>
-
-      
-    </div>
-  )
+    <>
+      <Hero />
+      {/* <Companies />
+      <Guide />
+      <Properties />
+      <Details />
+      <GetStarted /> */}
+      <Footer />
+    </>
+  );
 }
